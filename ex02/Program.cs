dde20214ef0d404e6c0ba6e1c598eb07df28fd62
MATHEUS FERREIRA DOS SANTOS <matheus.fsantos72@senacsp.edu.br>
2025-01-14 15:20:50 -0300

    first commit

---
+++ ex02/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Olá mundo! Sou Matheus");
+Console.WriteLine("Estou aprendendo lógica de programação");
+
---
+++ ex03/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Eu me chamo Matheus");
+Console.WriteLine("Estou estudando no Senac Largo Treze");
+
