0d3448ca3c57b8e7a76f97878c639550106ee401
MATHEUS FERREIRA DOS SANTOS <matheus.fsantos72@senacsp.edu.br>
2025-01-14 16:37:04 -0300

    Exercicio 04

---
+++ ex04/Program.cs
@@ -0,0 +1,14 @@
+﻿//Escreva um programa que pergunte o nome do usuário e apresente nome = boas vindas
+
+Console.WriteLine("Qual é o seu nome?");
+string nome = Console.ReadLine();
+
+Console.WriteLine("Qual é o seu sobrenome?");
+string sobrenome = Console.ReadLine();
+
+Console.WriteLine("Qual é o seu último nome?");
+string ultimosobrenome = Console.ReadLine();
+
+
+//Console.WriteLine("Seja bem-vindo(a) " + nome + sobrenome + ultimosobrenome);
+Console.WriteLine($"Seja bem-vindo(a)   {nome}  {sobrenome}  {ultimosobrenome} ");
--- ex04/obj/Debug/net8.0/ex04.AssemblyInfo.cs
+++ ex04/obj/Debug/net8.0/ex04.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ex03")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ex04")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dde20214ef0d404e6c0ba6e1c598eb07df28fd62")]
-[assembly: System.Reflection.AssemblyProductAttribute("ex03")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ex03")]
+[assembly: System.Reflection.AssemblyProductAttribute("ex04")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ex04")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
